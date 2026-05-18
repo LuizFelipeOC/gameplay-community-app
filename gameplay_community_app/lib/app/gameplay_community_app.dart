574d7d9ddbd18b98617982dc415e4b5f2a5b3a8a
_costa <luizfelipeeoliveiraac@gmail.com>
2026-05-18 19:52:01 -0400

    feat: start project

---
+++ gameplay_community_app/lib/app/gameplay_community_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GameplayCommunityApp extends StatefulWidget {
+  const GameplayCommunityApp({super.key});
+
+  @override
+  State<GameplayCommunityApp> createState() => _GameplayCommunityAppState();
+}
+
+class _GameplayCommunityAppState extends State<GameplayCommunityApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
---
+++ gameplay_community_app/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:gameplay_community_app/app/gameplay_community_app.dart';
+
+void main() {
+  runApp(GameplayCommunityApp());
+}
