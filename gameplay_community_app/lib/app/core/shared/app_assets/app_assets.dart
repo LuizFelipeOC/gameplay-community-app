152f65c95dca8bb89645eea1ab7f6eb625d8b4ca
_costa <luizfelipeeoliveiraac@gmail.com>
2026-05-18 20:08:34 -0400

    feat: configure svg assets app

---
+++ gameplay_community_app/lib/app/core/shared/app_assets/app_assets.dart
@@ -0,0 +1,8 @@
+final class AppAssets {
+  static const String happyPersonIcon = 'assets/svgs/happy_person_icon.svg';
+  static const String iconApp = 'assets/svgs/icon_app.svg';
+  static const String onboardingBackgroundImage = 'assets/svgs/onboarding_background_image.svg';
+  static const String onboardingScreenImage = 'assets/svgs/onboarding_screen_image.svg';
+  static const String rankedIcon = 'assets/svgs/ranked_icon.svg';
+  static const String swordIcon = 'assets/svgs/sword_icon.svg';
+}
