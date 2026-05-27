77469e7286f8fb67647aac89f8651278cb4a61e8
_costa <luizfelipeeoliveiraac@gmail.com>
2026-05-27 18:52:07 -0400

    feat: add route for FormCreateRoom screen in app router and define corresponding route constant

--- gameplay_community_app/lib/app/router/app_router.dart
+++ gameplay_community_app/lib/app/router/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:gameplay_community_app/app/features/auth/presentation/auth_screen.dart';
+import 'package:gameplay_community_app/app/features/form_create_room/presentation/form_create_room.dart';
 import 'package:gameplay_community_app/app/features/home/presentation/home_screen.dart';
 import 'package:gameplay_community_app/app/features/splash/presentation/splah_screen.dart';
 import 'package:gameplay_community_app/app/router/app_routes.dart';
@@ -9,5 +10,6 @@ final appRouter = GoRouter(
     GoRoute(path: AppRoutes.splashScreen, builder: (context, state) => const SplahScreen()),
     GoRoute(path: AppRoutes.auth, builder: (context, state) => const AuthScreen()),
     GoRoute(path: AppRoutes.home, builder: (context, state) => const HomeScreen()),
+    GoRoute(path: AppRoutes.formCreateRoom, builder: (context, state) => const FormCreateRoom()),
   ],
 );
--- gameplay_community_app/lib/app/router/app_routes.dart
+++ gameplay_community_app/lib/app/router/app_routes.dart
@@ -2,4 +2,5 @@ abstract interface class AppRoutes {
   static const String splashScreen = '/';
   static const String auth = '/auth';
   static const String home = '/home';
+  static const String formCreateRoom = '/form-create-room';
 }
