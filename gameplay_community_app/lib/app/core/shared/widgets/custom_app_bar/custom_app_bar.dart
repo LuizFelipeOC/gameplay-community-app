4e0b7ebd43d7fd970b3cb59dc66377565ea519e8
_costa <luizfelipeeoliveiraac@gmail.com>
2026-05-27 18:52:33 -0400

    feat: create CustomAppBar and FormCreateRoom components for improved UI structure and navigation

---
+++ gameplay_community_app/lib/app/core/shared/widgets/custom_app_bar/custom_app_bar.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:gameplay_community_app/app/core/shared/themes/colors/app_colors.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final GestureTapCallback onTap;
+
+  const CustomAppBar({super.key, required this.title, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    final backgroundColor = Theme.of(context).brightness == Brightness.light ? AppColors.surfaceLightHigh : AppColors.surfaceDarkHigh;
+    final textStyle = Theme.of(context).textTheme;
+
+    return Container(
+      padding: .only(top: 30),
+      height: preferredSize.height,
+      color: backgroundColor,
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Align(
+            alignment: Alignment.centerLeft,
+            child: IconButton(onPressed: onTap, icon: const Icon(Icons.chevron_left, size: 26)),
+          ),
+          Text(title, style: textStyle.headlineMedium, textAlign: TextAlign.center),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(140);
+}
---
+++ gameplay_community_app/lib/app/features/form_create_room/presentation/form_create_room.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:gameplay_community_app/app/core/shared/extensions/media_size_extension.dart';
+import 'package:gameplay_community_app/app/core/shared/widgets/background_scaffold/background_scaffold_widget.dart';
+import 'package:gameplay_community_app/app/core/shared/widgets/custom_app_bar/custom_app_bar.dart';
+import 'package:go_router/go_router.dart';
+
+class FormCreateRoom extends StatefulWidget {
+  const FormCreateRoom({super.key});
+
+  @override
+  State<FormCreateRoom> createState() => _FormCreateRoomState();
+}
+
+class _FormCreateRoomState extends State<FormCreateRoom> {
+  @override
+  Widget build(BuildContext context) {
+    final height = context.mediaSize.height;
+    final width = context.mediaSize.width;
+
+    return Scaffold(
+      appBar: CustomAppBar(title: 'Criar sala', onTap: context.pop),
+      body: BackgroundScaffoldWidget(
+        height: height,
+        width: width,
+        child: SingleChildScrollView(
+          child: Form(child: Column(children: [])),
+        ),
+      ),
+    );
+  }
+}
